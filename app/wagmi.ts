406596b6fa2284f20a27398d5444f6d04d2b020f
Dineshkumar A <i.medith1107@gmail.com>
2024-09-04 21:24:46 +0530

    chain ID

--- app/wagmi.ts
+++ app/wagmi.ts
@@ -1,14 +1,14 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-  polygonZkEvmCardona
+  baseSepolia
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'bc375bedd339481c19a1e23cac3fd380',
   chains: [
-    polygonZkEvmCardona,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [polygonZkEvmCardona] : []),
+    baseSepolia,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [baseSepolia] : []),
   ],
   ssr: true,
 });
